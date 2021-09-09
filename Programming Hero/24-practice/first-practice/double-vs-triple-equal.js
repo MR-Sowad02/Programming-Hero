9062dbe92eea848027e9aae1fef0046389655687
M R Sowad <71168875+MR-Sowad02@users.noreply.github.com>
2021-09-10 00:54:56 +0600

    Delete Programming Hero/24-practice/first-practice directory

--- Programming Hero/24-practice/first-practice/double-vs-triple-equal.js
+++
@@ -1,8 +0,0 @@
-const first = 2;
-const second = true;
-if(first == second){
-     console.log("condition is true");
-}
-else{
-     console.log("condition is false");
-}
--- Programming Hero/24-practice/first-practice/true-false.js
+++
@@ -1,8 +0,0 @@
-const love = "Gutipoka";
-
-if(love.length){
-     console.log("It's True");
-}
-else{
-     console.log("It's false");
-}    
